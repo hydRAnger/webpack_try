586d077592177d2c185f8a329879a8b10b58954e
hydRAnger <armyiljfe@gmail.com>
2015-04-17 20:22:36 +0800

    use minified react

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
 var config = {
   entry: path.resolve(__dirname, 'app/main.js'),
+  resolve: {
+    alias: {
+      'react': pathToReact
+    }
+  },
+
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
+
   module: {
     loaders: [{
       test: /\.jsx$/,
       loader: 'babel'
-    }]
+    }],
+
+    noParse: [pathToReact]
   }
 };
 
